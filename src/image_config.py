Fixed the ascii image output by fixing the scaling and font to pixel ratio. Result removed partial black border in output image.

src/image_config.py
@@ -9,11 +9,14 @@ class ImageConfig(object):
         self.new_width, self.new_height = 0, 0
 
     def convert_grayscale(self):
+        """
+        Converts colored R, G, B image to a grayscale image.
+        """
         return self.image.convert("L")
 
     def resize_image(self, width=200):
         """
-        Resizes image and will keep the aspect ratio.
+        Resizes image to a width of 200 pixels and will keep the aspect ratio of the original image.
         """
         self.new_width = width
         aspect_ratio = self.original_height/float(self.original_width)
@@ -23,17 +26,25 @@ class ImageConfig(object):
         return resized_image
 
     def get_new_width(self):
+        """
+        Returns the new width of the resized image.
+        """
         return self.new_width
 
     def get_new_height(self):
+        """
+        Returns the new height of the resized image.
+        """
         return self.new_height
 
     def get_pixel_matrix(self):
         """
-        Converts and image into tuples with the R, G, B code in each tuple.
+        Converts an image into tuples with the R, G, B value in each tuple.
         returns: a 2D array of R, G, B tuples
         """
         resized_image = self.resize_image()
         pixel_matrix = np.asarray(resized_image)
 
         return pixel_matrix
+
+